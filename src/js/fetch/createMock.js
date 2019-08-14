d402e1f65f51be2c4c8a900c38daeaf326e6f86f
hehuangz <hehuan@hzyushi.cn>
2019-08-14 16:02:40 +0800

    * 修改路由引用 - 去掉无用console

--- src/js/fetch/createMock.js
+++ src/js/fetch/createMock.js
@@ -10,15 +10,6 @@ function createMock (url, method, data, $this, cancel) {
     } catch (err) {
         console.error('请求mock数据失败：不存在 ' + lastName + '.json')
     }
-    // 输出请求信息
-    console.log('┏-----------------------------┓')
-    console.log('1. Request URL:')
-    console.log(url)
-    console.log('2. Request Form Data:')
-    console.log(data)
-    console.log('3. Response Data:')
-    console.log(res)
-    console.log('┗-----------------------------┛')
     return Promise.resolve(res)
 }
 // 本地调试 -- 建议使用mock数据 -- end
