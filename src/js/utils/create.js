b57eac1784bfe52704fb3b66c1204caa514a9931
hehuangz <hehuan@hzyushi.cn>
2020-03-03 09:47:39 +0800

    * extend

--- src/js/utils/create.js
+++ src/js/utils/create.js
@@ -1,20 +1,26 @@
 import Vue from 'vue'
+// import Notice from '@/components/Notice'
 function create (Component, props) {
-    const vm = new Vue({
-        render (h) {
-            // render方法将传入的实例转化为虚拟方法
-            return h(Component, { props })
-        }
-    }).$mount() // 执行挂载函数，但未指定挂载目标，表示只执行初始化工作
+    // 获取组件实例的方法有两个：
+    // 1.vue.extend()
+    const con = Vue.extend(Component)
+    const vm = new con({ propsData: props })
+    vm.$mount()
+    // 2.render()
+    // const vm = new Vue({
+    //     render (h) {
+    //         // render方法将传入的实例转化为虚拟方法
+    //         return h(Component, { props })
+    //     }
+    // }).$mount() // 执行挂载函数，但未指定挂载目标，表示只执行初始化工作
 
     document.body.appendChild(vm.$el)
-
-    const comp = vm.$children[0]
-    comp.remove = () => {
+    // const comp = vm.$children[0]
+    vm.remove = () => {
         document.body.removeChild(vm.$el)
         vm.$destroy()
     }
-    return comp
+    return vm
 }
 
 export default create
